d4aa1df9a91918d0da507a9c4b485a154649f1ef
Victor Cao <victorcao@live.ca>
2017-03-08 18:12:54 -0500

    Removed redundant assertions

--- test/javascript/test-server.js
+++ test/javascript/test-server.js
@@ -24,7 +24,6 @@ describe("All server testing", function () {
 				.put("/api/createroom/")
 				.send({roomPassword: "password", videoUrl: "random", screenName: "Mallory"})
 				.end(function (res) {
-					res.should.have.status(200);
 					res.body.should.have.property("roomname");
 					done();
 				});
@@ -36,13 +35,10 @@ describe("All server testing", function () {
 				.put("/api/createroom/")
 				.send({roomPassword: "password", videoUrl: "random", screenName: "Mallory"})
 				.end(function (res) {
-					res.should.have.status(200);
-					res.body.should.have.property("roomname");
 					chai.request(server)
 						.get("/api/session/")
 						.send({roomname: res.body.roomname, password: "password"})
 						.end(function (res2) {
-							res2.should.have.status(200);
 							res2.body.roomname.should.equal(res.body.roomname);
 							done();
 					});

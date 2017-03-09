155741583edaa09cc2544d9cd345216a921c719d
Victor Cao <victorcao@live.ca>
2017-03-08 21:07:50 -0500

    added test for 'play' signal.

--- test/javascript/test-server.js
+++ test/javascript/test-server.js
@@ -5,6 +5,13 @@ var chaiHttp = require("chai-http");
 var server = require("../../src/app.js");
 var should = chai.should();
 
+var io = require("socket.io-client");
+var socketUrl = "https://localhost:3000";
+var options = {
+    transports: ["websocket"],
+    "force new connection": true
+};
+
 chai.use(chaiHttp);
 
 describe("All server testing", function () {
@@ -46,4 +53,57 @@ describe("All server testing", function () {
         });
     });
 
+    describe("Test emit play", function() {
+        
+        var roomname;
+        beforeEach(function (){
+            chai.request(server)
+                .put("/api/createroom/")
+                .send({roomPassword: "password", videoUrl: "random", screenName: "Mallory"})
+                .end(function (res) {
+                    roomname = res.body.roomname;
+                });
+        });
+
+        it("should broadcast play to all users in the same room", function (done) {
+            var messageCounter = 0;
+
+            chai.request(server)
+                .get("/api/session/")
+                .send({roomname: roomname, password: "password"})
+                .end(function (res) {
+                    var personA = io.connect(socketUrl, options);
+                    personA.emit("join", {roomname: roomname, username: null});
+
+                    personA.on("error", function(msg) {
+                        console.log(msg);
+                    });
+
+                    personA.on("play", function(data) {
+                        messageCounter++;
+                        if (messageCounter === 2) {
+                            done();
+                        }
+                    });
+
+                    chai.request(server)
+                        .get("/api/session/")
+                        .send({roomname: roomname, password: "password"})
+                        .end(function (res2) {
+                            var personB = io.connect(socketUrl, options);
+                            personB.emit("join", {roomname: roomname, username: null});
+                            personB.on("play", function(data) {
+                                messageCounter++;
+                                if (messageCounter === 2) {
+                                    done();
+                                }
+                            });
+
+                            personB.on("userJoined", function (data) {
+                                personA.emit("play");
+                            });
+                        });
+                });
+        });
+    });
 });

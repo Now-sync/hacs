0260424c7f1cb1935f82e2b41f113f4b6d97acfe
Terrence Hung <terrencehung@hotmail.com>
2017-03-15 16:14:01 -0400

    fixed browser history undefined

--- src/frontend/js/client.js
+++ src/frontend/js/client.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Router, Route, browserHistory } from "react-router";
+import { Router, Route } from "react-router";
 import { syncHistoryWithStore } from 'react-router-redux';
+import { createBrowserHistory } from "history";
 
 import Layout from "./components/Layout";
 import store from "./store";
 
 const app = document.getElementById('app');
 
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(createBrowserHistory(), store);
 
 ReactDOM.render(
     <Provider store={store}>
